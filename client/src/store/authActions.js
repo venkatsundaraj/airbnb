48830473bfd7be430f461769443f579967c956f5
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-07-15 00:18:25 +0530

    client-vercel

--- client/src/store/authActions.js
+++ client/src/store/authActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:8000"
+const API_URL = "https://airbnb-sigma-six.vercel.app/"
 
 const registerAction = async function(userData){
     
