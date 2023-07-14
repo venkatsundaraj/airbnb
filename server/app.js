de97e3d6e3c38109ab52d77cd3aa468407149c3a
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-07-14 21:35:35 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const userRouter = require('./routes/userRoutes')
@@ -15,9 +16,23 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, "../client/dist")))
+
+    app.use("*",(req, res,next)=>{
+        res.sendFile(path.resolve(__dirname,"../client/dist/index.html"))
+    })
+}else{
+    app.use('/',(req,res,next)=>{
+        res.json('please set to production')
+    })
+}
+
 
 app.use(userRouter)
 app.use(hotelRouter)
+
+
 
 app.use(errorHandler)
 
