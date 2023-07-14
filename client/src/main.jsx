85dcbaff0a65896d2e8cdc4494f4590f32dabd7c
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-07-15 00:12:32 +0530

    vercel-1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+    <Provider store={store} basename="/">
       <BrowserRouter>
       <App />
       </BrowserRouter>
