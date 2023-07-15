2c1b5e30ec179b1d129352ee54269ae5d8219429
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-07-15 12:01:44 +0530

    vite98

--- client/src/store/authActions.js
+++ client/src/store/authActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:8000/admin"
+const API_URL = "https://airbnb-black-gamma.vercel.app/admin"
 
 const registerAction = async function(userData){
     
