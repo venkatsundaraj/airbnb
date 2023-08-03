072c94dd7341f00121c6fe9247eb3a57d9e0b8cf
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-08-03 15:28:46 +0530

    dummy-commit1

--- client/src/store/authActions.js
+++ client/src/store/authActions.js
@@ -1,41 +1,42 @@
 import axios from 'axios'
 
-const API_URL = "http://137.184.80.13/"
+const API_URL = "http://localhost:8000/"
+// const API_URL = "http://137.184.80.13/"
 
-const registerAction = async function(userData){
-    
-        const response = await axios.post(`${API_URL}/api/register`, userData)
+const registerAction = async function (userData) {
 
-        console.log(response.data)
-        
-        return  response.data
-
-    
-}
-
-const loginAction = async function(user){
-    const response = await axios.post(`${API_URL}/api/login`,user)
+    const response = await axios.post(`${API_URL}/api/register`, userData)
 
     console.log(response.data)
 
-    if(response.data.status==="ok"){
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+    return response.data
 
-    
-
-    return  response.data
 
 }
 
-const logoutAction = async function(){
+const loginAction = async function (user) {
+    const response = await axios.post(`${API_URL}/api/login`, user)
+
+    console.log(response.data)
+
+    if (response.data.status === "ok") {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+
+
+    return response.data
+
+}
+
+const logoutAction = async function () {
     return localStorage.removeItem('user')
 }
 
 const authServicesActions = {
-    registerAction:registerAction,
-    loginAction:loginAction,
-    logoutAction:logoutAction
+    registerAction: registerAction,
+    loginAction: loginAction,
+    logoutAction: logoutAction
 }
 
 export default authServicesActions
