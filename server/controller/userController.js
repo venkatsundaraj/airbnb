901196efc9012725c31b75d3f90c334e0bae966c
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-08-06 19:02:47 +0530

    dummy-commit1

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,84 +1,80 @@
-const asyncHandler = require('express-async-handler')
-const bcrypt = require('bcrypt')
-const User = require('../model/userModel')
-const jwt = require('jsonwebtoken')
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
-const generateJwt = function(id){
-    const token = jwt.sign({id:id},process.env.JWT_TOKEN, {
-        expiresIn:30 * 24* 60 * 60
-    })
-    return token
-}
+const generateJwt = function (id) {
+  const token = jwt.sign({ id: id }, process.env.JWT_TOKEN, {
+    expiresIn: 30 * 24 * 60 * 60,
+  });
+  return token;
+};
 
-exports.loginHandler = asyncHandler(async function(req,res,next){
-    try{const {email, password} = req.body
-    console.log(req.user)
+exports.loginHandler = asyncHandler(async function (req, res, next) {
+  try {
+    const { email, password } = req.body;
+    console.log(req.user);
 
-    if(!email || !password){
-        throw new Error('Please enter valid email and password')
+    if (!email || !password) {
+      throw new Error("Please enter valid email and password");
     }
-    const user = await User.findOne({email:email})
+    const user = await User.findOne({ email: email });
 
-    if(!user){
-        throw new Error('Cannot identify the user')
+    if (!user) {
+      throw new Error("Cannot identify the user");
     }
-    const decryptedPassword = await bcrypt.compare(password, user.password)
+    const decryptedPassword = await bcrypt.compare(password, user.password);
     // console.log(decryptedPassword)
 
-    if(decryptedPassword){
-        const loggedInUser = {
-        id:user._id,
-        userName:user.userName,
-        password:user.password,
-        email:user.email,
-        token:generateJwt(user._id)
+    if (decryptedPassword) {
+      const loggedInUser = {
+        id: user._id,
+        userName: user.userName,
+        password: user.password,
+        email: user.email,
+        token: generateJwt(user._id),
+      };
+
+      return res
+        .status(200)
+        .json({ user: loggedInUser, status: "ok", hello: "hello" });
+    } else {
+      return res
+        .status(401)
+        .json({ message: "please enter the correct password" });
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+exports.registerHandler = asyncHandler(async function (req, res, next) {
+  try {
+    const { userName, email, password, confirmPassword } = req.body;
+    console.log(req.body);
+
+    if (!userName || !email || !password) {
+      throw new Error("all fields are needed");
+    }
+    console.log(userName, email, password, confirmPassword);
+
+    const existedEmail = await User.findOne({ email: email });
+
+    if (existedEmail) {
+      throw new Error("User already exists");
+      // return res.status(400).json({message:'user already exist'})
     }
 
-    return res.status(200).json({user:loggedInUser, status:'ok'})
-    }else{
+    const encryptedPassword = await bcrypt.hash(password, 14);
 
-        return res.status(401).json({message:'please enter the correct password'})
-    }
+    const user = await User.create({
+      userName: userName,
+      email: email,
+      password: encryptedPassword,
+    });
 
-
-
-    
-
-}catch(err){
-    throw new Error(err)
-}
-})
-
-exports.registerHandler = asyncHandler(async function(req,res,next){
-    try{
-        const {userName, email, password, confirmPassword} = req.body
-        console.log(req.body)
-
-
-        if(!userName || !email || !password ){
-            throw new Error('all fields are needed')
-        }
-        console.log(userName, email, password, confirmPassword)
-
-        
-        const existedEmail = await User.findOne({email:email})
-
-        if(existedEmail){
-            throw new Error('User already exists')
-            // return res.status(400).json({message:'user already exist'})
-        }
-
-        const encryptedPassword = await bcrypt.hash(password, 14)
-
-        const user = await User.create({
-            userName:userName,
-            email:email, 
-            password:encryptedPassword
-        })
-
-        
-        return res.status(200).json(user)
-    }catch(err){
-        throw new Error(err)
-    }
-})
+    return res.status(200).json(user);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
